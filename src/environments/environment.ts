da82b67ee27dd0314ef5f23da99e619192e08f0d
sureshvarma1221 <suresh@opstronomy.com>
2024-09-28 18:42:42 +0530

    Add deployed server link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:3000' ,// Development API base URL
+    // baseUrl: 'http://localhost:3000' ,// local 
+    baseUrl:'https://seashell-app-rslco.ondigitalocean.app', // deployed
     routes:{
         createUser:"/api/users/create",
         getUsersList:"/api/users/all",
